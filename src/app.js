efaac1f885b9e712c6be224c657f9e76471d4d72
JuanRR17 <juanrepeto@gmail.com>
2022-08-13 11:43:10 +0200

    init

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+const $el = document.querySelector("#excuse > h1");
+const excuseParts = [who, action, what, when];
+
+const randomNumGen = max => {
+  return Math.floor(Math.random() * max);
+};
+
+const generateExcuse = () => {
+  return excuseParts.map(part => part[randomNumGen(part.length)]).join(" ");
+};
+
+window.addEventListener("click", () => {
+  $el.innerHTML = generateExcuse();
+});
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  $el.innerHTML = generateExcuse();
 };
